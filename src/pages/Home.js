57357fd0847085a8dbb434e35a713a1f771fae6a
BenStorms25 <65511280+BenStorms25@users.noreply.github.com>
2022-11-06 13:14:39 -0500

    topbar component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import CustomHeader from "../components/CustomHeader";
 import CustomNav from "../components/CustomNav";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home-page">
-      <CustomNav />
+      <CustomHeader />
     </div>
   );
 }
