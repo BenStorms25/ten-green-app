32d37cc9505729f022a59d7e27acb4e378603a62
BenStorms25 <ben.storms2@gmail.com>
2022-11-10 13:11:27 -0500

    working globe img

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,22 +1,26 @@
 import React, { useEffect } from "react";
 import tenGreenLogo from "../images/10Green Logo Black (1).png";
 import "./styles/HomeContent.css";
+import globeImage from "../images/10 Green Globe Green Cream 1.png";
 
 function HomeContent() {
   // scale the globe on scroll
 
-  function scaleGlobe() {
+  function scaleAndTransitionGlobe() {
     const globeDiv = document.querySelector("#globe");
+    const globeImage = document.querySelector("#globe-image");
 
     window.addEventListener("scroll", function () {
       // 2000 worked well for the scaling
       let scaler = window.scrollY / 2000;
+      let opacity = 1;
       if (window.scrollY < 850) {
         // 0 to 800 scrollY, scale globe
         globeDiv.setAttribute(
           "style",
           "transform: scale(" + (1 + scaler) + ");"
         );
+        globeImage.setAttribute("style", "opacity: " + { opacity } + ";");
       } else {
         // after 1200 scrollY, reverse process
         globeDiv.setAttribute(
@@ -30,7 +34,7 @@ function HomeContent() {
   }
 
   useEffect(() => {
-    scaleGlobe();
+    scaleAndTransitionGlobe();
   });
 
   return (
@@ -39,7 +43,9 @@ function HomeContent() {
         <img src={tenGreenLogo} alt=""></img>
       </div>
       <div className="globe-div">
-        <div id="globe"></div>
+        <div id="globe">
+          <img id="globe-image" src={globeImage} alt=""></img>
+        </div>
       </div>
     </>
   );
