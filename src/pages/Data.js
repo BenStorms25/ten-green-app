2df261b4fd904ef72dea7beee748a8c23a8198e9
Edaran123 <Edaran123>
2022-11-10 12:40:05 -0500

    Caught up to master branch

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import CustomHeader from "../components/CustomHeader";
 import DataContent from "../components/DataContent";
 import "./styles/Data.css";
+import "../Interactive-Map-Source/styles.css";
 import * as d3 from "d3";
 import MapApp from "../Interactive-Map-Source";
 
@@ -14,9 +15,7 @@ function Data() {
     <div className="data-page">
       <script src="https://d3js.org/d3.v7.min.js"></script>
       <CustomHeader />
-      <DataContent />
-      <p>Below is an example d3 program.</p>
-      <MapApp/>
+      <MapApp />
     </div>
   );
 }
