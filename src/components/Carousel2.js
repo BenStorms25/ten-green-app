c13bb1bb66ab5b472757e3d162826d2b4b696295
Edaran123 <Edaran123>
2022-11-14 18:21:12 -0500

    Fixed duplicate image display

--- src/components/Carousel2.js
+++ src/components/Carousel2.js
@@ -2,18 +2,18 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import {Swiper, SwiperSlide } from "swiper/react";
 import "../components/styles/Carousel2.css";
 import 'swiper/swiper.min.css'
-import 'swiper/modules/pagination/pagination.min.css'
+import 'swiper/swiper-bundle.css'
 
 
 import OfficeWorkerPicture from "../images/OfficeWorker.jpg";
-import ScienceGuyPicture from "../images/OfficeWorker.jpg";
+import ScienceGuyPicture from "../images/ScienceGuy.jpg";
 const Carousel2 = () => {
     return (
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
         loop={true}
-        slidesPerView={3}
+        slidesPerView={1}
         navigation
         //pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
