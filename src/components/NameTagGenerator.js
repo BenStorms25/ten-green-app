913f87a2b43cac0044a3172472a8436179fb55a1
Edaran123 <Edaran123>
2022-11-14 18:27:17 -0500

    Began work on generator for nametags

---
+++ src/components/NameTagGenerator.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import OfficeWorkerPicture from "../images/OfficeWorker.jpg";
+import ScienceGuyPicture from "../images/ScienceGuy.jpg";
+import KnightPicture from "../images/Knight.jpg"
+const ProfileArray = new Array();
+
+ProfileArray[0] = new Image();
+ProfileArray[0].src = OfficeWorkerPicture
+
+ProfileArray[1] = new Image();
+ProfileArray[1].src = ScienceGuyPicture
+
+ProfileArray[1] = new Image();
+ProfileArray[1].src = KnightPicture
