db51825f45e607fc94302146f4025a9612592025
Edaran123 <Edaran123>
2023-02-02 19:13:28 -0500

    Made default range 10 for graph

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,7 @@
 import React, { useEffect } from "react";
 import CustomHeader from "../components/CustomHeader";
-<<<<<<< HEAD
-import { GraphWidget } from "../components/GraphWidget";
-=======
+
 import AboutContent from "../components/AboutContent";
->>>>>>> b39510323f62bbc7b054ba6d0a51ada434cb79ed
 import "./styles/About.css";
 
 function About() {
@@ -15,12 +12,7 @@ function About() {
   return (
     <div className="about-page">
       <CustomHeader />
-<<<<<<< HEAD
-      <GraphWidget />
-
-=======
       <AboutContent />
->>>>>>> b39510323f62bbc7b054ba6d0a51ada434cb79ed
     </div>
   );
 }
