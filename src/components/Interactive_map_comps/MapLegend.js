671e69e4329a1c30e95dc8c051ae24a0d027c966
BenStorms25 <ben.storms2@gmail.com>
2023-02-06 17:06:33 -0500

    flipped legend

--- src/components/Interactive_map_comps/MapLegend.js
+++ src/components/Interactive_map_comps/MapLegend.js
@@ -7,74 +7,11 @@ function MapLegend() {
       <div className="score-card">
         <div
           className="score-stripe"
-          style={{ "background-color": "rgb(165, 0, 38)" }}
+          style={{ "background-color": "rgb(0, 104, 55)" }}
         >
           &nbsp;&nbsp;&nbsp;&nbsp;
         </div>
-        <p className="score-number">1</p>
-      </div>
-      <div className="score-card">
-        <div
-          className="score-stripe"
-          style={{ "background-color": "rgb(212, 50, 44)" }}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <p className="score-number">2</p>
-      </div>
-      <div className="score-card">
-        <div
-          className="score-stripe"
-          style={{ "background-color": "rgb(241, 110, 67)" }}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <p className="score-number">3</p>
-      </div>
-      <div className="score-card">
-        <div
-          className="score-stripe"
-          style={{ "background-color": "rgb(252, 172, 99)" }}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <p className="score-number">4</p>
-      </div>
-      <div className="score-card">
-        <div
-          className="score-stripe"
-          style={{ "background-color": "rgb(254, 221, 141)" }}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <p className="score-number">5</p>
-      </div>
-      <div className="score-card">
-        <div
-          className="score-stripe"
-          style={{ "background-color": "rgb(249, 247, 174)" }}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <p className="score-number">6</p>
-      </div>
-      <div className="score-card">
-        <div
-          className="score-stripe"
-          style={{ "background-color": "rgb(215, 238, 142)" }}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <p className="score-number">7</p>
-      </div>
-      <div className="score-card">
-        <div
-          className="score-stripe"
-          style={{ "background-color": "rgb(164, 216, 110)" }}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;
-        </div>
-        <p className="score-number">8</p>
+        <p className="score-number">10</p>
       </div>
       <div className="score-card">
         <div
@@ -88,11 +25,74 @@ function MapLegend() {
       <div className="score-card">
         <div
           className="score-stripe"
-          style={{ "background-color": "rgb(0, 104, 55)" }}
+          style={{ "background-color": "rgb(164, 216, 110)" }}
         >
           &nbsp;&nbsp;&nbsp;&nbsp;
         </div>
-        <p className="score-number">10</p>
+        <p className="score-number">8</p>
+      </div>
+      <div className="score-card">
+        <div
+          className="score-stripe"
+          style={{ "background-color": "rgb(215, 238, 142)" }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <p className="score-number">7</p>
+      </div>
+      <div className="score-card">
+        <div
+          className="score-stripe"
+          style={{ "background-color": "rgb(249, 247, 174)" }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <p className="score-number">6</p>
+      </div>
+      <div className="score-card">
+        <div
+          className="score-stripe"
+          style={{ "background-color": "rgb(254, 221, 141)" }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <p className="score-number">5</p>
+      </div>
+      <div className="score-card">
+        <div
+          className="score-stripe"
+          style={{ "background-color": "rgb(252, 172, 99)" }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <p className="score-number">4</p>
+      </div>
+      <div className="score-card">
+        <div
+          className="score-stripe"
+          style={{ "background-color": "rgb(241, 110, 67)" }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <p className="score-number">3</p>
+      </div>
+      <div className="score-card">
+        <div
+          className="score-stripe"
+          style={{ "background-color": "rgb(212, 50, 44)" }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <p className="score-number">2</p>
+      </div>
+      <div className="score-card">
+        <div
+          className="score-stripe"
+          style={{ "background-color": "rgb(165, 0, 38)" }}
+        >
+          &nbsp;&nbsp;&nbsp;&nbsp;
+        </div>
+        <p className="score-number">1</p>
       </div>
     </div>
   );
