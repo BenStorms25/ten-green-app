fc9f6ddffc637b016bbe92eefd8c8a93e19fce31
Tereza Holubcová <terezaholubcova@host-0800-f84d89601e48.pool.net.maine.edu>
2023-02-28 18:56:05 -0500

    pollutants cards-finished

--- src/components/PollutantsContent.js
+++ src/components/PollutantsContent.js
@@ -18,100 +18,89 @@ function PollutantsContent() {
     <>
       <div className="center-content-1">
         <h1>Pollutants & Standards</h1>
-        <div className="pollutant-cards">
-          <div class="centered">
-            <section class="cards">
-              <article class="card">
-                <img src={co2Image}></img>
-                <p>Carbon Monoxide</p>
-                <a href="pollutants-and-standards/co">
-                  <button>Learn More</button>
-                </a>
-              </article>
+        <section class="cards">
+          <article class="card">
+            <img src={co2Image}></img>
+            <p>CO</p>
+            <div class="dropdown">
+              <button>Learn More</button>
+            </div>
+          </article>
 
-              <article class="card">
-                <img src={noImage}></img>
-                <p>Nitric Oxide</p>
-                <a href="pollutants-and-standards/no">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={smallParticulateMatterImage}></img>
+            <p>PM10</p>
+            <a href="pollutants-and-standards/small-particulates">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={ozoneImage}></img>
-                <p>Ozone</p>
-                <a href="pollutants-and-standards/ozone">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={largeParticulateMatterImage}></img>
+            <p>PM2.5</p>
+            <a href="pollutants-and-standards/small-particulates">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={sulfurDioxideImage}></img>
-                <p>Sulfur Dioxide</p>
-                <a href="pollutants-and-standards/so2">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={sulfurDioxideImage}></img>
+            <p>SO2</p>
+            <a href="pollutants-and-standards/so2">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={nitrogenDioxideImage}></img>
-                <p>Nitrogen Dioxide</p>
-                <a href="pollutants-and-standards/no2">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={nitrogenDioxideImage}></img>
+            <p>NO2</p>
+            <a href="pollutants-and-standards/no2">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={nickelImage}></img>
-                <p>Nickel</p>
-                <a href="pollutants-and-standards/nickel">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={noImage}></img>
+            <p>NO</p>
+            <a href="pollutants-and-standards/no">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={leadImage}></img>
-                <p>Lead</p>
-                <a href="pollutants-and-standards/lead">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={ozoneImage}></img>
+            <p>Ozone</p>
+            <a href="pollutants-and-standards/ozone">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={cadmiumImage}></img>
-                <p>Cadmium</p>
-                <a href="pollutants-and-standards/cadmium">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={leadImage}></img>
+            <p>Lead</p>
+            <a href="pollutants-and-standards/lead">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={arsenicImage}></img>
-                <p>Arsenic</p>
-                <a href="pollutants-and-standards/arsenic">
-                  <button>Learn More</button>
-                </a>
-              </article>
+          <article class="card">
+            <img src={cadmiumImage}></img>
+            <p>Cadmium</p>
+            <a href="pollutants-and-standards/cadmium">
+              <button>Learn More</button>
+            </a>
+          </article>
 
-              <article class="card">
-                <img src={smallParticulateMatterImage}></img>
-                <p>Small Particulate Matter</p>
-                <a href="pollutants-and-standards/small-particulates">
-                  <button>Learn More</button>
-                </a>
-              </article>
-
-              <article class="card">
-                <img src={largeParticulateMatterImage}></img>
-                <p>Large Particulate Matter</p>
-                <a href="pollutants-and-standards/large-particulates">
-                  <button>Learn More</button>
-                </a>
-              </article>
-            </section>
-          </div>
-        </div>
+          <article class="card">
+            <img src={arsenicImage}></img>
+            <p>Arsenic</p>
+            <a href="pollutants-and-standards/arsenic">
+              <button>Learn More</button>
+            </a>
+          </article>
+        </section>
       </div>
+
       <CustomFooter />
     </>
   );
