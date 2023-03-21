6c4908049830b90c06f4925b4b0d5cac532bdb98
Edaran123 <Edaran123>
2023-03-21 18:12:09 -0400

    Made graph shorter

--- src/Homepage-map/Marks.js
+++ src/Homepage-map/Marks.js
@@ -46,7 +46,6 @@ export const Marks = ({ UsaGeo, data, year, colorScale, maximum }) => {
                 : "grey"
             }
           >
-            
             <title>
               
               {dataMap.get(feature.id)} {"- "}
