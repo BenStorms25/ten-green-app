b18d3e53d24a64dfb2024bc1be54754aab525861
Edaran123 <Edaran123>
2023-04-21 14:14:15 -0400

    Changed download link to source link

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -147,7 +147,7 @@ function HomeContent() {
         </div>
       </div>
       <Globe dataPoints={dataPoints} current_graph_max={current_graph_max} />
-      <a href="http://204.197.4.170/10green/zip_download" class="downloadbtn">Download Data</a>
+      <a href="https://aqs.epa.gov/aqsweb/airdata/download_files.html#Annual" class="sourcebtn">Source: EPA Annual Air Data</a>
     </>
   );
 }
